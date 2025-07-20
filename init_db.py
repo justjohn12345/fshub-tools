1aed2b63f4c4c7b8774b2c86f174de1f4508150f
John Newlin <jnewlin@mac.lan>
2025-07-20 10:08:08 -0700

    Tool to create empty database.

---
+++ init_db.py
@@ -0,0 +1,32 @@
+
+import sqlite3
+
+conn = sqlite3.connect('fshub.db')
+c = conn.cursor()
+
+# Create table
+c.execute('''
+CREATE TABLE flights (
+    flightid INTEGER PRIMARY KEY,
+    pilotid INTEGER,
+    pilotname TEXT,
+    landing_rate REAL,
+    ts DATETIME,
+    distance INTEGER,
+    "time" INTEGER,
+    aircraft_icao TEXT,
+    aircraft_name TEXT,
+    departure_icao TEXT,
+    arrival_icao TEXT,
+    fuel_used REAL,
+    departure_time DATETIME,
+    arrival_time DATETIME
+)
+''')
+
+c.execute('CREATE INDEX idx_ts ON flights (ts)')
+
+conn.commit()
+conn.close()
+
+print("Database initialized.")
