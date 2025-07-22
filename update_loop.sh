73073fe1d4c3b1a92c1f2c8cc6e39d8da1a68267
John Newlin <jnewlin@mac.lan>
2025-07-22 14:00:56 -0700

    Use relative path to update script

--- update_loop.sh
+++ update_loop.sh
@@ -3,7 +3,7 @@
 while true
 do
   echo "Running updatedb.py..."
-  python3 /Users/jnewlin/src/venvs/fshub-tools/updatedb.py
+  python3 updatedb.py
   echo "Sleeping for 5 minutes..."
   sleep 300
 done
